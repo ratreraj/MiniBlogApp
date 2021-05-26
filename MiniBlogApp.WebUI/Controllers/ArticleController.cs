18e33e1d93771a2ab530690f6b678003ff6c4b77
raj.ratre <raj.ratre@KCDESK-GR150>
2021-05-26 22:04:13 +0530

    Summer Notes Image Upload

---
+++ MiniBlogApp.WebUI/Controllers/ArticleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniBlogApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniBlogApp.WebUI.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Create(ArticleModel model)
+        {
+            ModelState.Remove("Id");
+            if (ModelState.IsValid)
+            { 
+            
+            
+            }
+            return Json(model);
+        }
+
+
+        
+    }
+}
